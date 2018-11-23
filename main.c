2f43f2a659898c98cc88657218e4a08a4fd485c2
John Kindem <461425614@qq.com>
2018-11-23 15:38:28 +0800

    [fix]

--- main.c
+++ main.c
@@ -228,7 +228,7 @@ int ping(char *addressArg, int n, int l) {
 
                         // 输出结果
                         printf(
-                            "%d byte from %s: icmp_seq=%u ttl=%d rtt=%.3f\n",
+                            "%d byte from %s: icmp_seq=%u ttl=%d rtt=%.3fms\n",
                             l + 8,
                             inet_ntoa(ipHeader->ip_src),
                             icmpHeader->icmp_seq,
