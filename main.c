e7b4655b96d2469e7b2ffe18a96a0da1afdbf7b9
John Kindem <461425614@qq.com>
2018-11-23 14:43:25 +0800

    [fix]

--- main.c
+++ main.c
@@ -137,7 +137,7 @@ int ping(char *addressArg, int n, int l) {
         // 封装 icmp 包
         assembleIcmpPackage((struct icmp *) sendBuffer, ++count, l, pid);
         // 发送 icmp 包
-        if (sendto(sock, sendBuffer, l, 0, (struct sockaddr *) &address, sizeof(address)) < 0) {
+        if (sendto(sock, sendBuffer, l + 8, 0, (struct sockaddr *) &address, sizeof(address)) < 0) {
             printf("Send data fail.\n");
             continue;
         }
@@ -278,8 +278,7 @@ unsigned short getCheckSum(unsigned short *header, int length) {
 
     // 如果最后剩下一个字节，补齐两个字节，继续累加
     if (count == 1) {
-        * (unsigned char *) (&result) = *(unsigned char *) t;
-        sum += result;
+        sum += * (unsigned char *) t;
     }
 
     sum = (sum >> 16) + (sum & 0xffff);
